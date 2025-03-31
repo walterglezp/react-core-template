09943d8809d1aad7c56360f80f8ed6fa19f00720
Walter Gonzalez <walterglezp@gmail.com>
2025-03-31 14:55:19 -0400

    Created NotFoundPage to handle unknown routes

---
+++ src/pages/not-found/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div className="not-found-page">
+      <h1>404 - Page Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from '@pages/home'
+import NotFoundPage from '@pages/not-found'
 import { ROUTES } from '@app/router/routes'
 
 const AppRouter = () => {
@@ -10,6 +11,7 @@ const AppRouter = () => {
         {/* Defaults */}
         <Route path="/" element={<HomePage />} />
         <Route path={ROUTES.home} element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   )
